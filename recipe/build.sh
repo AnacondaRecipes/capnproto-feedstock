2e3f26496a7c2d0954f999580a96f5758fd55838
Andrii Osipov <aosipov@anaconda.com>
2026-07-17 06:38:50 -0700

    review fixes

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,24 +9,8 @@ if [[ $target_platform == "linux-"* ]]; then
     export LIBS="-lrt $LIBS"
 fi
 
-BUILD_ARCH=${CONDA_TOOLCHAIN_BUILD%%-*}
-HOST_ARCH=${CONDA_TOOLCHAIN_HOST%%-*}
+./configure --enable-shared --prefix=$PREFIX
 
-configure_cmd=(./configure --enable-shared --prefix=$PREFIX)
-
-# cross-compiling needs to use prebuilt capnproto
-# https://github.com/capnproto/capnproto/issues/1815#issuecomment-1732327995
-if [[ "${BUILD_ARCH}" != "${HOST_ARCH}" ]]; then
-    configure_cmd+=("--with-external-capnp")
-fi
-
-"${configure_cmd[@]}"
-
-make_cmd=(make -j${CPU_COUNT})
-if [[ "${BUILD_ARCH}" == "${HOST_ARCH}" ]]; then
-    make_cmd+=(check)
-fi
-
-"${make_cmd[@]}"
+make -j${CPU_COUNT} check
 
 make install
