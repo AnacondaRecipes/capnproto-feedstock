e982f8404da8191c36243272be1190651e19aae1
Andrii-Osipov <aosipov@anaconda.com>
2026-07-17 16:52:35 -0700

    capnproto 1.5.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,26 +2,14 @@
 
 set -x
 
-if [[ $target_platform == "osx-"* ]]; then
-    echo "try to replace config.*"
-    list_config_to_patch=$(find . -name config.guess | sed -E 's/config.guess//')
-    for config_folder in $list_config_to_patch; do
-        echo "copying config to $config_folder ...\n"
-        cp -v $BUILD_PREFIX/share/libtool/build-aux/config.* $config_folder
-    done
-fi
+cd c++
+autoreconf -vfi
 
 if [[ $target_platform == "linux-"* ]]; then
-   export LIBS="-lrt $LIBS"
+    export LIBS="-lrt $LIBS"
 fi
 
-CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}" \
-./configure --host=${HOST} \
-    --enable-shared \
-    --prefix=$PREFIX
-
-# So that the tests pass
-mkdir $PREFIX/ssl/certs
+./configure --enable-shared --prefix=$PREFIX
 
 make -j${CPU_COUNT} check
 
